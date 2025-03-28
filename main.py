40928308a65ade66914da55fe0f12a0e03df8009
Dima-tyman <tyman-dima@list.ru>
2025-03-28 16:27:34 +0300

    New stage

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
-import time
+# import pythoncom
+# from win32com.client import gencache, Dispatch
+#
+# kompas6_constants = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
+# kompas6_constants_3d = gencache.EnsureModule("{2CAF168C-7961-4B90-9DA2-701419BEEFE3}", 0, 1, 0).constants
+#
+# kompas_api7_module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
+# kompas_api7_module.IApplication(Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(kompas_api7_module.IApplication.CLSID, pythoncom.IID_IDispatch))
+
+# print(type(kompas_api7_module))
+
 import pythoncom
 from win32com.client import Dispatch, gencache
-from win32com.server.policy import regPolicy
 
+module7 = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
+api7 = module7.IApplication(Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(module7.IApplication.CLSID, pythoncom.IID_IDispatch))
+const7 = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
+api7.Visible = True
+print(type(api7))
+doc2D = api7.ActiveDocument
+print(type(doc2D))
 
-def get_kompas_api7():
-    module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
-    api = module.IKompasAPIObject(
-        Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(module.IKompasAPIObject.CLSID,
-                                                                 pythoncom.IID_IDispatch))
-    const = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
-    return module, api, const
-
-module7, api7, const7 = get_kompas_api7()  # Подключаемся к API7
-app7 = api7.Application  # Получаем основной интерфейс
-docs = app7.Documents
-app7.Visible = True  # Показываем окно пользователю (если скрыто)
-# app7.HideMessage = const7.ksHideMessageNo  # Отвечаем НЕТ на любые вопросы программы
-print(app7.ApplicationName(FullName=True))  # Печатаем название программы
-
-# while True:
-print("Hello")
-activeDoc = app7.ActiveDocument
-
-KompasDocument2D = activeDoc._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D'], pythoncom.IID_IDispatch)
+KompasDocument2D = doc2D._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D'], pythoncom.IID_IDispatch)
+KompasDocument2D1 = doc2D._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D1'], pythoncom.IID_IDispatch)
 doc2D = module7.IKompasDocument2D(KompasDocument2D)
 views = doc2D.ViewsAndLayersManager.Views
-if views(1) is not None:
-    views(1).Delete()
-    print("DELETE")
-
-View = views.Add(1)._oleobj_.QueryInterface(module7.NamesToIIDMap['IView'], pythoncom.IID_IDispatch)
-view = module7.IView(View)
-
-view.Scale = 2
-view.Name = "Test2"
-view.Number = 5
+AssociationView = views.Add(3)._oleobj_.QueryInterface(module7.NamesToIIDMap['IAssociationView'], pythoncom.IID_IDispatch)
+view = module7.IAssociationView(AssociationView)
+view.SourceFileName = "D:\\KOMPAS SDK\\Parts\\test1.m3d"
+view.Unfold = True
 view.Update()
+print(type(doc2D))
+doc2D = module7.IKompasDocument2D1(KompasDocument2D1)
+print(type(doc2D))
+doc2D.RebuildDocument()
