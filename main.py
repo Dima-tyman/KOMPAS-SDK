7b3bf26c50d063675456b4defd4db1d05a292128
Dima-tyman <tyman-dima@list.ru>
2025-03-31 10:26:52 +0300

    First work version 1.0

--- main.py
+++ main.py
@@ -1,35 +1,59 @@
-# import pythoncom
-# from win32com.client import gencache, Dispatch
-#
-# kompas6_constants = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
-# kompas6_constants_3d = gencache.EnsureModule("{2CAF168C-7961-4B90-9DA2-701419BEEFE3}", 0, 1, 0).constants
-#
-# kompas_api7_module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
-# kompas_api7_module.IApplication(Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(kompas_api7_module.IApplication.CLSID, pythoncom.IID_IDispatch))
-
-# print(type(kompas_api7_module))
-
+import time
 import pythoncom
 from win32com.client import Dispatch, gencache
 
+
+PATH = "D:\\KOMPAS SDK\\Parts\\test1.m3d"
+DXFPATH = "D:\\KOMPAS SDK\\Parts\\test1.dxf"
+
+
+# Application init
 module7 = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
 api7 = module7.IApplication(Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(module7.IApplication.CLSID, pythoncom.IID_IDispatch))
 const7 = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
 api7.Visible = True
-print(type(api7))
-doc2D = api7.ActiveDocument
-print(type(doc2D))
+api7.HideMessage = const7.ksHideMessageYes
 
-KompasDocument2D = doc2D._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D'], pythoncom.IID_IDispatch)
-KompasDocument2D1 = doc2D._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D1'], pythoncom.IID_IDispatch)
-doc2D = module7.IKompasDocument2D(KompasDocument2D)
+# Documents init and create
+doc = api7.Documents.Add(1)
+IKompasDocument2D = doc._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D'], pythoncom.IID_IDispatch)
+doc2D = module7.IKompasDocument2D(IKompasDocument2D)
+IKompasDocument2D1 = doc._oleobj_.QueryInterface(module7.NamesToIIDMap['IKompasDocument2D1'], pythoncom.IID_IDispatch)
+doc2D1 = module7.IKompasDocument2D1(IKompasDocument2D1)
+
+# Change document sheet format
+sheet = doc.LayoutSheets(0)
+sheet.LayoutLibraryFileName = "C:\\Program Files\\ASCON\\KOMPAS-3D v22\\Sys\\graphic.lyt"
+sheet.LayoutStyleNumber = 15 # 13 - not into frame, 15 - empty sheet
+sheet.Update()
+
+# Views init and create
 views = doc2D.ViewsAndLayersManager.Views
-AssociationView = views.Add(3)._oleobj_.QueryInterface(module7.NamesToIIDMap['IAssociationView'], pythoncom.IID_IDispatch)
-view = module7.IAssociationView(AssociationView)
-view.SourceFileName = "D:\\KOMPAS SDK\\Parts\\test1.m3d"
-view.Unfold = True
+view = views.Add(3)
+IAssociationView = view._oleobj_.QueryInterface(module7.NamesToIIDMap['IAssociationView'], pythoncom.IID_IDispatch)
+viewAssoc = module7.IAssociationView(IAssociationView)
+IAssociationViewElements = view._oleobj_.QueryInterface(module7.NamesToIIDMap['IAssociationViewElements'], pythoncom.IID_IDispatch)
+viewAssocElement = module7.IAssociationViewElements(IAssociationViewElements)
+IViewDesignation = view._oleobj_.QueryInterface(module7.NamesToIIDMap['IViewDesignation'], pythoncom.IID_IDispatch)
+viewDesign = module7.IViewDesignation(IViewDesignation)
+
+# Add view parameter
+viewAssoc.SourceFileName = PATH
+viewAssoc.ProjectionName = "#Развертка"
 view.Update()
-print(type(doc2D))
-doc2D = module7.IKompasDocument2D1(KompasDocument2D1)
-print(type(doc2D))
-doc2D.RebuildDocument()
+viewAssoc.Unfold = True
+viewAssocElement.CreateCentresMarkers = False
+viewAssocElement.CreateCircularCentres = False
+viewAssocElement.CreateLinearCentres = False
+viewDesign.ShowUnfold = False
+viewDesign.ShowScale = False
+view.Update()
+doc2D1.RebuildDocument()
+
+# Save to DXF
+doc.SaveAs(DXFPATH)
+
+# Clear document / for test
+time.sleep(1)
+view.Delete()
+doc.Close(0)
